2ae127baac1f7d46c7284918b55ef19119546cfc
Caitlyn <localcmail@gmail.com>
2021-05-08 10:23:08 +0300

    Emoji manager download progress implemented

--- NdvBot/Discord/Commands/EmojiManager/EmojiManager.cs
+++ NdvBot/Discord/Commands/EmojiManager/EmojiManager.cs
@@ -7,38 +7,81 @@ using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using System.Timers;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using NdvBot.Utils;
 
 namespace NdvBot.Discord.Commands.EmojiManager
 {
     public class EmojiManager : BaseCommandModule
     {
+
+        private static string GetUrlForProgress(ulong emojiId, string emojiName)
+        {
+            return emojiId + "_" + emojiName;
+        }
+        
         [Command("emojiDownload")]
         [DSharpPlus.CommandsNext.Attributes.Description("Downloads all emojis from server and packs them into archive. Usage: [downloadGifs = true]")]
         public async Task Download(CommandContext ctx, bool downloadGifs = true)
         {
             var emojis = ctx.Guild.Emojis.Where(e => downloadGifs || !e.Value.IsAnimated);
+            if (ctx.Guild.Emojis.Count <= 0)
+            {
+                return;
+            }
+
+            var downloadProgress = new ConcurrentDictionary<string, int>();
+            var emojisList = emojis.ToList();
+            foreach (var (_, emoji) in emojisList)
+            {
+                downloadProgress.TryAdd(EmojiManager.GetUrlForProgress(emoji.Id, emoji.Name), 0);
+            }
+
+            var msg = await ctx.RespondAsync("Download progress: ");        
+            var progressTimer = new Timer(1000); // because of discord ratelimit
+            var stringProgressBar = new StringProgressBar(50, "Download progress: [", "]", '#');
+            progressTimer.Enabled = true;
+            progressTimer.AutoReset = true;
+            progressTimer.Elapsed += async (_, _) =>
+            {
+                stringProgressBar.SetProgress(downloadProgress.Sum(e => e.Value) / downloadProgress.Count);
+                await msg.ModifyAsync(stringProgressBar.ToString());
+            };
+            progressTimer.Start();
+
             var emotesData = new ConcurrentDictionary<string, byte[]>();
             var tasks = new List<Task>();
-            foreach (var emoji in emojis)
+            foreach (var emoji in emojisList)
             {
-                Task<byte[]> DownloadEmote(string url)
+                Task<byte[]> DownloadEmote(string url, string urlForProgress)
                 {
                     using var client = new WebClient();
+                    client.DownloadProgressChanged += (o, args) =>
+                    {
+                        if (!downloadProgress.TryGetValue(urlForProgress, out var progress))
+                        {
+                            downloadProgress.TryAdd(urlForProgress, args.ProgressPercentage);
+                        }
+                        else
+                        {
+                            downloadProgress.TryUpdate(urlForProgress, args.ProgressPercentage, progress);
+                        }
+                    };
                     return client.DownloadDataTaskAsync(new Uri(url));
                 }
-                tasks.Add(DownloadEmote(emoji.Value.Url).ContinueWith(res =>
+                tasks.Add(DownloadEmote(emoji.Value.Url, EmojiManager.GetUrlForProgress(emoji.Value.Id, emoji.Value.Name)).ContinueWith(res =>
                 {
                     var splitByDot = emoji.Value.Url.Split(".");
                     emotesData.TryAdd(emoji.Value.Name + "." + splitByDot[splitByDot.Length - 1], res.Result);
                 }));
             }
-
             await Task.WhenAll(tasks);
             tasks.Clear();
+            progressTimer.Stop();
             
             await using var zipStream = new MemoryStream();
             using var zipArchive = new ZipArchive(zipStream, ZipArchiveMode.Create, true);
@@ -52,8 +95,11 @@ namespace NdvBot.Discord.Commands.EmojiManager
 
 
             zipStream.Position = 0;
-            await new DiscordMessageBuilder().WithFiles(
-                new Dictionary<string, Stream>() {{"emojis.zip", zipStream}}).SendAsync(ctx.Channel);
+            var builder =  new DiscordMessageBuilder().WithFiles(
+                new Dictionary<string, Stream>() {{"emojis.zip", zipStream}});
+            var t1 =  msg.DeleteAsync();
+            var t2 = ctx.RespondAsync(builder);
+            await Task.WhenAll(t1, t2);
         }
 
         [Command("deleteEmojis")]
---
+++ NdvBot/Utils/StringProgressBar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace NdvBot.Utils
+{
+    public class StringProgressBar
+    {
+        private readonly StringBuilder _builder;
+        private readonly string _prefix;
+        private readonly string _postfix;
+
+        private readonly int _effectiveLength;
+        private readonly char _progressSymbol;
+        
+        private double _progress;
+
+        public StringProgressBar(int progressBarLength, string prefix, string postfix, char progressSymbol)
+        {
+            this._builder = new();
+            this._effectiveLength = progressBarLength - prefix.Length - postfix.Length;
+            this._prefix = prefix;
+            this._postfix = postfix;
+            this._progressSymbol = progressSymbol;
+        }
+
+        public void SetProgress(int progress)
+        {
+            this._progress = progress / 100d;
+        }
+
+        public override string ToString()
+        {
+            this._builder.Clear();
+            this.FillBuilder(this._builder);
+            return this._builder.ToString();
+        }
+
+        public StringBuilder ToStringBuilder()
+        {
+            var builder = new StringBuilder();
+            this.FillBuilder(builder);
+            return builder;
+        }
+        
+        private void FillBuilder(StringBuilder builder)
+        {
+            builder.Append(this._prefix);
+            var progressSymbols = (int)Math.Round(this._progress * this._effectiveLength);
+            builder.Append(new string(this._progressSymbol, progressSymbols));
+            builder.Append(new string(' ', this._effectiveLength - progressSymbols));
+            builder.Append(this._postfix);
+        }
+    }
+}
